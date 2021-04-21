fd8843bb5475d93e2f689ef0cd15b5e178cd1ada
Liam <81374620+LiamMCode@users.noreply.github.com>
2021-04-21 19:09:02 +0100

    Implemented the Hex colour project
    - added the next project to the website - had to use a separate Js file due to the eventListener from the colour change button project - added in some comments

---
+++ js/hex.js
@@ -0,0 +1,25 @@
+//used to fix Js errors to do with const and document not defined
+/*eslint-env es6*/
+/*eslint-env browser*/
+
+(function() {
+    const button = document.querySelector('#btn')
+    const body = document.querySelector('body')
+    const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+    const value = document.querySelector('#hex-value')
+    
+    button.addEventListener('click', changeHex)
+    
+    function changeHex()
+    {
+        let hex ='#'
+        
+        for (let i =0; i < 6; i++)
+        {
+            const index = Math.floor(Math.random()*hexValues.length)
+            hex += hexValues[index]
+        }
+        value.textContent = hex
+        body.style.backgroundColor = hex
+    }
+} )()
