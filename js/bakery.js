fb2606de925ed26b31db7130c7d9ab6d97a48d1a
Liam <81374620+LiamMCode@users.noreply.github.com>
2021-04-26 23:21:18 +0100

    Wrote the Js
    - Js for bakery written - CSS for navbar added - still need to write CSS for the bakery part of the page

---
+++ js/bakery.js
@@ -0,0 +1,53 @@
+//used to fix Js errors to do with const and document not defined
+/*eslint-env es6*/
+/*eslint-env browser*/
+
+(function(){
+    const buttons = document.querySelectorAll('.btn')
+    const shopItems = document.querySelectorAll('.shop-item')
+    
+    buttons.forEach((button)=> {
+        button.addEventListener('click', (e) => {
+            e.preventDefault()
+            const filter = e.target.dataset.filter
+            
+            shopItems.forEach((item)=> {
+                if (filter === 'all')
+                {
+                    item.style.display ='block'        
+                }
+                else
+                {
+                    if (item.classList.contains(filter))
+                    {
+                        item.style.display = 'block'   
+                    }
+                    else 
+                    {
+                        item.style.display = 'none'   
+                    }    
+                }
+            })
+        })
+    })
+})();
+
+(function(){
+    const searchBox = document.querySelector('#search-item')
+    const shopItems = document.querySelectorAll('.shop-item')
+    
+    searchBox.addEventListener('keyup', (e) => {
+        const searchFilter = e.target.value.toLowerCase().trim()
+        
+        shopItems.forEach((item) => {
+            if (item.textContent.includes(searchFilter))
+            {
+                item.style.display = 'block'
+            }
+            else
+            {
+                item.style.display = 'none'
+            }
+        })
+    })
+})();
